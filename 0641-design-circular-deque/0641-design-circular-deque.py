debfc1df3cffc2e2411d531c01625069320ad0ad
Venkata Sai Teja Bodapati <79343859+bvsteja117@users.noreply.github.com>
2025-01-01 10:31:18 +0530

    Time: 11 ms (32.65%), Space: 18.7 MB (17.68%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -1,106 +1,58 @@
-class MyCircularDeque(object):
-
-    def __init__(self, k):
-        """
-        :type k: int
-        """
+class MyCircularDeque:
+    def __init__(self, k: int):
+        """Initialize your data structure here. Set the size of the deque to be k."""
+        self.k = k
         self.q = [0] * k
-        self.front = 0
         self.size = 0
-        self.capacity = k
-        
+        self.front = 0
+        self.rear = k - 1
 
-    def insertFront(self, value):
-        """
-        :type value: int
-        :rtype: bool
-        """
+    def insertFront(self, value: int) -> bool:
+        """Adds an item at the front of Deque. Return true if the operation is successful."""
         if self.isFull():
             return False
-        if not self.isEmpty():
-            self.front = (self.front - 1 + self.capacity) % self.capacity
+        self.front = (self.front - 1 + self.k) % self.k
         self.q[self.front] = value
         self.size += 1
         return True
-        
 
-    def insertLast(self, value):
-        """
-        :type value: int
-        :rtype: bool
-        """
+    def insertLast(self, value: int) -> bool:
+        """Adds an item at the rear of Deque. Return true if the operation is successful."""
         if self.isFull():
             return False
-        idx = (self.front + self.size) % self.capacity
-        self.q[idx] = value
+        self.rear = (self.rear + 1) % self.k
+        self.q[self.rear] = value
         self.size += 1
         return True
-        
 
-    def deleteFront(self):
-        """
-        :rtype: bool
-        """
+    def deleteFront(self) -> bool:
+        """Deletes an item from the front of Deque. Return true if the operation is successful."""
         if self.isEmpty():
             return False
-        self.front = (self.front + 1) % self.capacity
-        # basically, welcome to overwirte it behind frond
+        self.front = (self.front + 1) % self.k
         self.size -= 1
         return True
-        
 
-    def deleteLast(self):
-        """
-        :rtype: bool
-        """
+    def deleteLast(self) -> bool:
+        """Deletes an item from the rear of Deque. Return true if the operation is successful."""
         if self.isEmpty():
             return False
-        # basically, welcome to overwirte
+        self.rear = (self.rear - 1 + self.k) % self.k
         self.size -= 1
         return True
-        
 
-    def getFront(self):
-        """
-        :rtype: int
-        """
-        if self.isEmpty():
-            return -1
-        return self.q[self.front]
-        
+    def getFront(self) -> int:
+        """Get the front item from the deque."""
+        return -1 if self.isEmpty() else self.q[self.front]
 
-    def getRear(self):
-        """
-        :rtype: int
-        """
-        if self.isEmpty():
-            return -1
-        idx = (self.front + self.size - 1) % self.capacity
-        return self.q[idx]
+    def getRear(self) -> int:
+        """Get the last item from the deque."""
+        return -1 if self.isEmpty() else self.q[self.rear]
 
-    def isEmpty(self):
-        """
-        :rtype: bool
-        """
+    def isEmpty(self) -> bool:
+        """Checks whether the circular deque is empty or not."""
         return self.size == 0
 
-        
-
-    def isFull(self):
-        """
-        :rtype: bool
-        """
-        return self.size == self.capacity
-        
-
-
-# Your MyCircularDeque object will be instantiated and called as such:
-# obj = MyCircularDeque(k)
-# param_1 = obj.insertFront(value)
-# param_2 = obj.insertLast(value)
-# param_3 = obj.deleteFront()
-# param_4 = obj.deleteLast()
-# param_5 = obj.getFront()
-# param_6 = obj.getRear()
-# param_7 = obj.isEmpty()
-# param_8 = obj.isFull()
+    def isFull(self) -> bool:
+        """Checks whether the circular deque is full or not."""
+        return self.size == self.k
